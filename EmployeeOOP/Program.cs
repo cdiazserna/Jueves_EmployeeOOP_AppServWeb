7010d842caf9b70f29ed964c0693d611acde67e7
Carlor Jr Diaz <cjunior90@gmail.com>
2023-09-09 15:47:29 -0500

    New EmployeeOOP project created with class Date.cs

---
+++ EmployeeOOP/Program.cs
@@ -0,0 +1,32 @@
+﻿using EmployeeOOP.Classes;
+
+try
+{
+    //Declaración de variables
+    int day, month, year;
+
+    Console.WriteLine("OOP APPLICATION");
+    Console.WriteLine("---------------");
+
+    Console.Write("Ingresar el día: ");
+    day = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Ingresar el mes: ");
+    month = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Ingresar el año: ");
+    year = Convert.ToInt32(Console.ReadLine());
+
+    //Inicialización de la clase Date
+    Date dateObject = new Date(day, month, year);
+    Console.WriteLine("\n");
+    Console.WriteLine(dateObject.ToString());
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+
+
