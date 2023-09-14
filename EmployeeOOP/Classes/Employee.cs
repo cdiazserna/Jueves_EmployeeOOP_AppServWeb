89b3a30abc74940a0da2926f3089a642fa494c95
Carlor Jr Diaz <cjunior90@gmail.com>
2023-09-14 18:39:29 -0500

    New abstract class Employee created

---
+++ EmployeeOOP/Classes/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeOOP.Classes
+{
+    public abstract class Employee
+    {
+        #region Properties
+        public int Id { get; set; } //123
+        public string? FirstName { get; set; } // Maria
+        public string? LastName { get; set; } //Perez
+        public Date? BirthDate { get; set; } // 15/05/2000
+        public Date? HiringDate { get; set; } // 14/09/2023
+        public bool? IsActive { get; set; } // true
+        #endregion
+
+        #region Methods
+
+        public abstract decimal GetValueToPay();
+
+        public override string ToString()
+        {
+            return $"INFORMACIÓN EMPLEADO \n\t " +
+                $"ID: {Id}\n\t" +
+                $"Nombres: {FirstName}\n\t" +
+                $"Apellidos: {LastName}\n\t" +
+                $"Fecha de Cumpleaños: {BirthDate}\n\t" +
+                $"Fecha de Contratación: {HiringDate}\n\t" +
+                $"¿Empleado activo?: {IsActive}\n\t";
+        }
+
+        #endregion
+    }
+}
